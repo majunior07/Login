a8b88cb6b26f050caabcbd70ef3b0f510052b471
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-07-14 09:09:02 -0300

    Primeiro commit 14/07/2023

---
+++ src/contexts/Auth/AuthProvider.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { User } from "../../types/User";
+import { AuthContext } from "./AuthContext"
+
+
+export const AuthProvider = ({ children } : { children: JSX.Element}) => {
+    
+    const [user, setUser] = useState<User | null>(null);
+
+    const signin = (email: string, password: string) => {
+
+    }
+
+    const signout = () => {
+        
+    }
+
+    return(
+        <AuthContext.Provider>
+            { children }
+        </AuthContext.Provider>
+    );
+}
